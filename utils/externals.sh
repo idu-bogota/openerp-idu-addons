5462e4c08650e6ae3a5304a2af48d36a1190979f
Cinxgler Mariaca M <cinxgler@gmail.com>
2012-10-17 08:49:27 -0500

    removing submodules to use script externals.sh

--- utils/externals.sh
+++ utils/externals.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd ~/git/openerp-idu-addons/externals/
+
 #zend framework
 #svn co http://framework.zend.com/svn/framework/standard/tags/release-1.12.0/library/ zend
 ZF_RELEASE='ZendFramework-1.12.0'
@@ -6,3 +8,9 @@ wget "http://packages.zendframework.com/releases/$ZF_RELEASE/$ZF_RELEASE-minimal
 tar zxvf "$ZF_RELEASE-minimal.tar.gz"
 mv "$ZF_RELEASE-minimal" zend
 rm "$ZF_RELEASE-minimal.tar.gz"
+
+#La version de eclipse que viene en ubuntu 12.04 no maneja apropiadamente los submodules,
+#por lo tanto este script se va a usar para descargar las dependencias.
+
+git clone https://github.com/shiflett/testmore.git testmore-php
+
