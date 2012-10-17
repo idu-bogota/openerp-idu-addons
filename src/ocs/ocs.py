4d5f4fdfa7cd57a3903b0bc27b01830cb42015c4
Andres Ignacio Baez <andresignaciob@gmail.com>
2012-10-16 20:02:28 -0500

    Se adiciona reglas en workflow

--- src/ocs/ocs.py
+++ src/ocs/ocs.py
@@ -23,21 +23,21 @@ from osv import fields,osv
 from base_geoengine import geo_model
 from datetime import datetime
 from datetime import timedelta
+from crm import crm
+from crm_claim import crm_claim
+
+class Res_Partner(osv.osv):
+    """Add Partner Identificator for Colomb        ia """
+    _name="res.partner"
+    _inherit="res.partner"
+    _columns={
+        'partner_identificator':fields.char('Company Identificator',size=64,help="Useful in Colombia to Store (NIT-Numero de Identificacion Tributaria)")
+     }
 
 class ResPartnerAddress(geo_model.GeoModel):
     """This class inherhits from res.partner.address"""
     def name_get(self, cr, user, ids, context=None):
         """Get Full Name of Citizen """        
-        """       
-        res = {}
-        for citizen in self.browse(cr, user, ids, context = context):
-            if citizen.last_name==False:
-                res[citizen.id] = "{0}".format(citizen.name)
-            else:
-                res[citizen.id] = "{0},{1}".format(citizen.last_name, citizen.name)             
-        return  res
-                
-        """
         if context is None:
             context = {}
         if not len(ids):
@@ -63,11 +63,26 @@ class ResPartnerAddress(geo_model.GeoModel):
         """Get Full Name of Citizen """
         res = {}
         for citizen in self.browse(cr, uid, ids, context = context):
-            if citizen.last_name==False:
+            if citizen.last_name == False:
                 res[citizen.id] = "{0}".format(citizen.name)
             else:
                 res[citizen.id] = "{0},{1}".format(citizen.last_name, citizen.name)             
         return  res
+    
+    
+    def _checkinputdata(self, cr, uid, ids, context = None):
+        """
+        This constraint allow evaluate at least one of these data.         
+        """        
+        is_valid_data = False
+        for citizen in self.browse(cr,uid,ids,context=None):
+            if ((citizen.email != False)|
+                (citizen.twitter != False)|
+                (citizen.phone != False)|
+                (citizen.facebook != False)|
+                (citizen.mobile != False)):                    
+                is_valid_data = True                    
+        return is_valid_data
     
     _name = 'res.partner.address'
     _inherit='res.partner.address' 
@@ -82,12 +97,17 @@ class ResPartnerAddress(geo_model.GeoModel):
         'district_id':fields.many2one('ocs.district','District'),        
         'neighborhood_id':fields.many2one('ocs.neighborhood','Neighborhood'),
         'full_name':fields.function(_get_full_name,type='char',string='Full Name',method=True),
-        'geo_point':fields.geo_point('Location',srid=4668,readonly=True),         
+        'geo_point':fields.geo_point('Location',srid=4668,readonly=True),
+        #'issue_comment': fields.one2many('cso.issue_comment','issue_id','Comments',help="comment to issue"),
+        'claim_id':fields.one2many('crm.claim','id','Historic of Claims',help="Claims opened by User")         
     } 
     _rec_name = 'document_id'          
     _order= "last_name asc"
     _sql_constraints = [
         ('unique_cc_document_id','unique(document_type,document_id)','Combination document type, document id must be unique!!!')    
+    ]   
+    _constraints = [
+     (_checkinputdata,'You must type at least one of these: email, phone, cell phone, facebook or twitter to create a contact',['document_id']),
     ]
      
 ResPartnerAddress()
@@ -100,12 +120,25 @@ class ocs_citizen_service_point(geo_model.GeoModel):
         'name': fields.char('Description',size=128,help='Description of Citizen Service Point point',required=True),   
         'creation_date': fields.datetime('Creation Date',help='Date when Citizen Atention Point is installed'),        
         'close_date': fields.datetime('End Date',help='When citizen Atention Point is closed'),
-        'schedule': fields.char('Schedule',size=60,help='For example L-V 8:30 am -12:50 pm'),
+        'schedule': fields.char('Schedule',size=60,help='For example L-V 8:30 am -12:50 pm'),       
         'geo_point':fields.geo_point('Location',srid=4668,readonly=True),
     }
 ocs_citizen_service_point()
 
-
+class ocs_claim_classification(osv.osv):
+    """This field contains internal classification for claims """
+    _name="ocs.claim_classification"
+    _columns={
+      'id':fields.integer('ID',readonly=True),
+      'code':fields.char('Code',size=6),
+      'name':fields.char('Name',size=128),
+      'enabled':fields.boolean('Enabled',help='If item is valid now'),
+      'parent_id':fields.many2one('ocs.claim_classification','Parent')
+    }    
+    constraints = [
+        (osv.osv._check_recursion,'Error ! You cannot create recursive Claim Classification Category',['parent_id'])
+    ]    
+ocs_claim_classification()
 
 class ocs_neighborhood(geo_model.GeoModel):
     """Contains geographic information about all towns in the city"""
@@ -127,7 +160,6 @@ class ocs_district(geo_model.GeoModel):
     }
 ocs_district()
 
-
 class ocs_sub_district(geo_model.GeoModel):
     """ Contains geografic information about especific territories """
     _name = 'ocs.sub_district'
@@ -138,33 +170,25 @@ class ocs_sub_district(geo_model.GeoModel):
     } 
 ocs_sub_district()
 
-
-
 class crm_claim(geo_model.GeoModel):
    _name = "crm.claim"
-   _inherit = "crm.claim"
-   
+   _inherit = "crm.claim"   
    def _get_full_name(self,cr,uid,ids,fieldname,arg,context=None):
         """Get Full Name of Citizen """
         res = {}
         for citizen in self.browse(cr, uid, ids, context = context):
                 res[citizen.id] = "{0}/{1} ".format(citizen.classification_id.name,citizen.sub_classification_id.name)                         
-        return  res
-   
+        return  res   
    def _get_channel(self, cr, uid, context=None):
        obj = self.pool.get('ocs.input_channel')
        ids = obj.search(cr, uid, [])
        res = obj.read(cr, uid, ids, ['name', 'id'], context)
        res = [(r['id'], r['name']) for r in res]
-       return res
-   
+       return res   
    def _get_main_classification(self, cr, uid, context=None):
        """This function get the main Categories
       Select name,id from ocs_claim_classification where 
-      parent_id = null  
-      
-        
-        } 
+      parent_id = null 
       """
        obj = self.pool.get('ocs.claim_classification_id')
        ids = obj.search(cr, uid, [('parent_id','=',False)])
@@ -176,7 +200,7 @@ class crm_claim(geo_model.GeoModel):
         'priority': fields.selection([('h','High'),('n','Normal'),('l','Low')], 'Priority', required=True),
         'external_id':fields.char('External ID',size=128,help='External Claim System Identificator'),
         'external_dms_id': fields.char('External DMS ID',size=20,help='External Document Management System Identificator'),
-        'csp_id':fields.many2one('ocs.citizen_service_point','CSP',help='Citizen Service Point',required=True),
+        'csp_id':fields.many2one('ocs.citizen_service_point','CSP',domain="[('close_date','=',False)]",help='Citizen Service Point',required=True),
         'channel':fields.many2one('crm.case.channel','Case Channel',help='Case Channel',required=True),
         'categ_id': fields.many2one('crm.case.categ', 'Category', \
                             domain="[('section_id','=',section_id),\
@@ -193,40 +217,27 @@ class crm_claim(geo_model.GeoModel):
     }   
    _rec_name = 'classification'
    #time.strftime('%Y-%m-%d %H:%M:%S')
-   
    _defaults = {
         'date_deadline': lambda *a:  (datetime.now()+timedelta(days=9)).__format__('%Y-%m-%d %H:%M:%S'),
         'priority': lambda *a: 'l'
-        }   
- 
+        }
+   
+   def test_response(self, cr, uid, ids, *args):
+       """
+       Check if Response Text is Empty      
+       """       
+       isResponsed = False
+       for claim in self.browse(cr,uid,ids,context=None):
+           response = claim.resolution
+           if response == False:
+               isResponsed = False
+               message = "Resolution could not be Empty"
+           else:
+               isResponsed = True
+               message = "The claim: {0} -- has been closed".format(claim.name)
+       self.log(cr, uid, claim.id, message)
+       return isResponsed
    
 crm_claim()
 
 
-class ocs_claim_classification(osv.osv):
-    """This field contains internal classification for claims """
-    _name="ocs.claim_classification"
-    _columns={
-      'id':fields.integer('ID',readonly=True),
-      'code':fields.char('Code',size=6),
-      'name':fields.char('Name',size=128),
-      'enabled':fields.boolean('Enabled',help='If item is valid now'),
-      'parent_id':fields.many2one('ocs.claim_classification','Parent')
-    }
-    
-    constraints = [
-        (osv.osv._check_recursion,'Error ! You cannot create recursive Claim Classification Category',['parent_id'])
-    ]
-    
-ocs_claim_classification()
-
-class Res_Partner(osv.osv):
-    """Add Partner Identificator for Colombia """
-    _name="res.partner"
-    _inherit="res.partner"
-    _columns={
-        'partner_identificator':fields.char('Company Identificator',size=64,help="Useful in Colombia to Store (NIT-Numero de Identificacion Tributaria)")
-     }
-
-
-    
