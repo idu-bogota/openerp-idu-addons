dc066e46f62bef671e25dd612681fc15b3a8bd0b
Andres Ignacio Baez <andresignaciob@gmail.com>
2012-11-14 14:49:11 -0500

    Punto de atencion al ciudadano esta activo cuando la fecha de cierre o es nula o menor a la fecha actual

--- src/ocs/ocs.py
+++ src/ocs/ocs.py
@@ -281,7 +281,7 @@ class crm_claim(geo_model.GeoModel):
          'priority': fields.selection([('h','High'),('n','Normal'),('l','Low')], 'Priority', required=True, readonly=True,states={'draft':[('readonly',False)],'open':[('readonly',False)]}),
          'external_id':fields.char('External ID',size=128,help='External Claim System Identificator',readonly=True,states={'draft':[('readonly',False)],'open':[('readonly',False)]}),
          'external_dms_id': fields.char('External DMS ID',size=20,help='External Document Management System Identificator',readonly=True,states={'draft':[('readonly',False)],'open':[('readonly',False)]}),
-         'csp_id':fields.many2one('ocs.citizen_service_point','CSP',domain="[('close_date','=',False)]",help='Citizen Service Point',required=True,readonly=True,states={'draft':[('readonly',False)],'open':[('readonly',False)]}),
+         'csp_id':fields.many2one('ocs.citizen_service_point','CSP',help='Citizen Service Point',required=True,readonly=True,states={'draft':[('readonly',False)],'open':[('readonly',False)]}),
          'channel':fields.many2one('crm.case.channel','Case Channel',help='Case Channel',required=True,readonly=True,states={'draft':[('readonly',False)],'open':[('readonly',False)]}),
          'categ_id': fields.many2one('crm.case.categ', 'Category', \
                              domain="[('section_id','=',section_id),\
