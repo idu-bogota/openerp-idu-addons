210407fb0fb5be216a801394eb750424c2db928f
Cinxgler Mariaca M <cinxgler@gmail.com>
2012-11-21 16:31:27 -0500

    Release openerp6.1-rev2012111901

--- src/data_idu/__openerp__.py
+++ src/data_idu/__openerp__.py
@@ -1,6 +1,6 @@
 {
   'name': 'Módulo para crear datos del IDU',
-  'version': 'openerp6.1-rev2012111901',
+  'version': 'openerp6.1-rev2012112100',
   'category': 'Generic Modules/Others',
   'description': "Módulo para cargar datos usados en el IDU",
   'author': 'STRT',
