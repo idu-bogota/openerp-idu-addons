335ab2e78ed9dbe72ffd2fa7fcc9a3236a2d2584
Andres Ignacio Baez <andresignaciob@gmail.com>
2013-03-07 15:12:18 -0500

    Se deja lo mejor posible el repositorio de geocodificacion

---
+++ src/ocs_idu/geocoder/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    INSTITUTO DE DESARROLLO URBANO IDU
+#    PLAN DE DESARROLLO 
+#    BOGOTA HUMANA 2012-2016
+#    Alcalde Mayor: Gustavo Petro Urrego
+#    Director IDU: Maria Fernanda Rojas
+#    Director STRT: Angel María Fonseca Correa
+#    Arquitecto: Cinxgler Mariaca Minda
+#    Programador: Andres Ignacio Báez Alba
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import geocode
