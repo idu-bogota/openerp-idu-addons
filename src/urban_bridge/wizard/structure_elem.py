cb50ffaee94909e9d40393642eef4717173c3850
andresignaciob <andresignaciob@gmail.com>
2013-06-21 22:37:23 -0500

    El EAV guarda en la base de datos correctamente

--- src/urban_bridge/wizard/structure_elem.py
+++ src/urban_bridge/wizard/structure_elem.py
@@ -79,49 +79,8 @@ class urban_bridge_wizard_structure_elem(osv.osv_memory):
         res.update({'elem_id':str_element.id})
         return res
 
-    def on_change_structure_elem_type(self,cr,uid,ids,elem_type,context=None):
-        """
-        Set Visible only the fields defined by Structure Element Type
-        """
-#        res = {}
-#        elem_type_obj=self.pool.get('urban_bridge.structure_element_type') 
-#        for elem_type in elem_type_obj.browse(cr,uid,elem_type):
-#            for attr in elem_type.attributes:
-#                field_id = elem_type.id+"_"+attr.id
-#                res[field_id]
-        #res = super(urban_bridge_wizard_structure_elem, self).default_get(cr, uid, fields, context=context)
-        
-        
-#        elem_type=res.elem_type
-#        attr = elem_type.attributes
-#        for att in elem_type:
-#            att_name = att.name
-#            att_datatype = att.datatype
-        return []
-
     def elem_create (self, cr, uid, ids, context=None):
-#        wsdl_url = self.pool.get('ir.config_parameter').get_param(cr, uid, 'orfeo.ws.url', default='', context=context)
-#        client = Client(wsdl_url)
-#
-#        current_user = self.pool.get('res.users').browse(cr, uid, uid, context=context)
-#        claim_ids = context and context.get('active_ids', False)
-#        claim_table = self.pool.get('crm.claim')
-#
-#        form_object = self.browse(cr, uid, form_object_id, context=context)
-#        asunto = form_object.description.strip()
-#        if(len(asunto) > 300):
-#            raise osv.except_osv('Error de validación','El texto a ser radicado es demasiado largo, por favor resumalo')
-#        dependenciaDestino = form_object.dependencia_id.code
-#
-#        usuarioLogin = current_user.login
-#        tipoRadicacion = 2 #Radicados de entrada
-#        municipio = 1 # Bogotá
-#        departamento = 11 # Distrito Capital
-#        pais = 170 # Colomba
-#        continente = 1 # América
-         form_object_id = ids and ids[0] or False
-         form_object = self.browse(cr, uid, form_object_id, context=context)
-         return {'type': 'ir.actions.act_window_close'}
+        return {'type': 'ir.actions.act_window_close'}
 
 
     def create(self, cr, uid, vals, context=None):
@@ -131,28 +90,23 @@ class urban_bridge_wizard_structure_elem(osv.osv_memory):
         if context is None: context = {}
         #1. Se obtiene el valor de elem_id
         elem_id = context['active_id'] 
-        
+        context['elem_type_vals']=vals
         
         #2. Se crea el registro de que se hizo un inventario
         urban_bridge_wizard_structure_elem_id = super(urban_bridge_wizard_structure_elem,self).create(cr, uid, {'elem_id': elem_id}, context=context)
         #3. Se ejemplifica el objeto structure_elem que se invocó y se miran los valores que tiene con el fin de asignarles un valor que se va 
         #a actualizar.
         structure_elem_obj=self.pool.get('urban_bridge.structure_element')
-        structure_elem = structure_elem_obj.browse(cr,uid,elem_id)
-        id = structure_elem.id
-        bridge_id = structure_elem.bridge_id
         structure_elem_value_obj=self.pool.get('urban_bridge.structure_element_value')
+        structure_elem_attribute_obj=self.pool.get('urban_bridge.structure_element_attribute')
+        structure_elem = structure_elem_obj.browse(cr,uid,elem_id)
         #Se itera sobre los elementos recibidos
         #Si no existen se crean
         for value_form in vals:
             if not (str(value_form).startswith("n") or str(value_form).startswith("e")):#Values aren't name or structure_elem
                 s = value_form.split('_')
                 struct_elem_attribute_id = s[1]
-                attribute = self.pool.get('urban_bridge.structure_element_attribute').browse(cr,uid,struct_elem_attribute_id)
-                if attribute is False:
-                    break
-                
-                
+                attribute = structure_elem_attribute_obj.browse(cr,uid,int(struct_elem_attribute_id))
                 data_type = attribute.data_type
                 #4. Se arma el diccionario que se va a pasar al metodo
                 str_elem_val_vals={}
@@ -178,7 +132,7 @@ class urban_bridge_wizard_structure_elem(osv.osv_memory):
                     if (struc_elem_value.element_attribute_id == struct_elem_attribute_id):
                         id_value=struc_elem_value.id
                         isnew=False
-                if(isnew):
+                if (not isnew):
                     #Write
                     structure_elem_value_obj.write(cr,uid,id_value,str_elem_val_vals)
                 else:
