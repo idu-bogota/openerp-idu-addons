35fa6602c2e811c413ea44119ac702e74487eb54
Juanfedo <julian.fernandez@idu.gov.co>
2013-06-27 16:13:17 -0500

    Mis cambios de urban_pavement

--- src/urban_pavement/urban_pavement.py
+++ src/urban_pavement/urban_pavement.py
@@ -18,19 +18,51 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
 #
 ##############################################################################
-from osv import fields
+from osv import fields,osv
 from base_geoengine import geo_model
 
-class urban_pavement_roadway(geo_model.GeoModel):
-    """ 
-    Roadway Base Polygon
+class urban_pavement_evaluator(osv.osv):
     """
-    _name="urban_pavement.roadway"
+        Structure Type Main Classification
+    """
+    _name="urban_pavement.evaluator"
     _columns={
-        'shape':fields.geo_multi_polygon('Coordinate'),
-        'id':fields.integer('Polygon ID'),
-        'ric':fields.integer('Rodway Idenfication Code'),#CIV Codigo de Identificacion de la Via
-        'create_date':fields.date('Create Date'),
-        'adherence':fields.float('Adherence',digits=(12,6)),
+        'name':fields.char('Name :',size=256),
+        'identification':fields.char('Identification :',size=20),
     }
-urban_pavement_roadway()
+
+urban_pavement_evaluator()
+
+class urban_pavement_evaluation(geo_model.GeoModel):
+    """
+        Structure Type Main Classification
+    """
+    _name="urban_pavement.evaluation"
+    _columns={
+        'civ': fields.char('CIV',size = 20),
+        'pci': fields.integer('PCI'),
+        'eval_date': fields.date('Date Evaluation'),
+        'evaluator_id': fields.many2one('urban_pavement.evaluator',"Evaluator",required = True),
+        'photo':fields.binary('Photo'),
+        'component_id':fields.one2many('urban_pavement.component','component_id',required = True),
+        'shape':fields.geo_multi_polygon('Shape'),
+    }
+   
+urban_pavement_evaluation()
+
+class urban_pavement_component(osv.osv):
+    """
+        Structure Type Main Classification
+    """
+    _name="urban_pavement.component"
+    _columns={
+        'damage':fields.float('Damage :'),
+        'extension':fields.float('Extension :'),
+        'severity':fields.integer('Severity :'),
+        'component_id':fields.integer('component_id'),
+    }
+ 
+urban_pavement_component()
+
+
+
