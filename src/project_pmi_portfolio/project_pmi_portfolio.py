d1d92e7425e1b093cb024f94fb31bbef295dd94b
Cinxgler Mariaca <cinxgler@gmail.com>
2013-09-04 19:30:24 -0500

    Adicionado project_pmi_portfolio

---
+++ src/project_pmi_portfolio/project_pmi_portfolio.py
@@ -0,0 +1,87 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2013 Instituto de Desarrollo Urbano (<http://www.idu.gov.co>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, osv
+
+class project(osv.osv):
+    _name = "project.project"
+    _inherit = "project.project"
+
+project()
+
+class project_pmi_portfolio(osv.osv):
+    _name = "project_pmi.portfolio"
+    _description = "Handles a portfolio tree"
+
+    def name_get(self, cr, uid, ids, context=None):
+        if isinstance(ids, (list, tuple)) and not len(ids):
+            return []
+        if isinstance(ids, (long, int)):
+            ids = [ids]
+        reads = self.read(cr, uid, ids, ['name','parent_id'], context=context)
+        res = []
+        for record in reads:
+            name = record['name']
+            if record['parent_id']:
+                name = record['parent_id'][1]+' / '+name
+            res.append((record['id'], name))
+        return res
+
+    def _name_get_fnc(self, cr, uid, ids, prop, unknow_none, context=None):
+        res = self.name_get(cr, uid, ids, context=context)
+        return dict(res)
+
+    _columns = {
+        'name': fields.char('Name', size=64, required=True, select=True),
+        'complete_name': fields.function(_name_get_fnc, type="char", string='Name'),
+        'parent_id': fields.many2one('project_pmi.portfolio','Parent portfolio', select=True, ondelete='cascade'),
+        'child_id': fields.one2many('project_pmi.portfolio', 'parent_id', string='Child Portfolios'),
+        'sequence': fields.integer('Sequence', select=True, help="Gives the sequence order when displaying a list."),
+        'children_type': fields.selection([('portfolio','Contain portfolios only'), ('portfolio_project','Contain projects and portfolios')], 'Portfolio Children Type', required=True),
+        'parent_left': fields.integer('Left Parent', select=1),
+        'parent_right': fields.integer('Right Parent', select=1),
+    }
+
+    _defaults = {
+        'children_type' : lambda *a : 'portfolio_project',
+    }
+
+    _parent_name = "parent_id"
+    _parent_store = True
+    _parent_order = 'sequence, name'
+    _order = 'parent_left'
+
+    def _check_recursion(self, cr, uid, ids, context=None):
+        level = 100
+        while len(ids):
+            cr.execute('select distinct parent_id from project_pmi_portfolio where id IN %s',(tuple(ids),))
+            ids = filter(None, map(lambda x:x[0], cr.fetchall()))
+            if not level:
+                return False
+            level -= 1
+        return True
+
+    _constraints = [
+        (_check_recursion, 'Error ! You cannot create recursive portfolios.', ['parent_id'])
+    ]
+    def child_get(self, cr, uid, ids):
+        return [ids]
+
+project_pmi_portfolio()
