9081e802841f6c2f1f7df9189252cec72f10a62d
Cinxgler Mariaca <cinxgler@gmail.com>
2013-11-07 19:49:05 -0500

    Refactorin en los modelos para hacerlo más simple

--- src/project_pmi_wbs/project_pmi_wbs.py
+++ src/project_pmi_wbs/project_pmi_wbs.py
@@ -41,6 +41,7 @@ class project(osv.osv):
 
 project()
 
+#TODO: Eliminar este objeto y dejar solo uno
 class project_pmi_wbs(osv.osv):
     _name = "project_pmi.wbs"
     _inherit = ['mail.thread']
@@ -72,10 +73,21 @@ class project_pmi_wbs(osv.osv):
 
 project_pmi_wbs()
 
+#TODO: Adicionar vista Gantt a WBS
+#TODO: Validar que un work package no tenga child_ids
+#TODO: Validar work_package tiene unidad de medida
+#TODO: Validar que los pesos de un mismo nivel den el 100%
+#TODO: Adicionar vista kamban para los work_packages y adicionar link en la vista kanban de proyectos 
+#TODO: Relacionar Tareas con Work Packages
 class project_pmi_deliverable(osv.osv):
     _name = "project_pmi.deliverable"
     _inherit = ['mail.thread']
     _description = "Defines a deliverables tree that belongs to a WBS"
+
+    _parent_name = "parent_id"
+    _parent_store = True
+    _parent_order = 'sequence, name'
+    _order = 'parent_id DESC'
 
     def name_get(self, cr, uid, ids, context=None):
         if isinstance(ids, (list, tuple)) and not len(ids):
@@ -96,30 +108,32 @@ class project_pmi_deliverable(osv.osv):
         return dict(res)
 
     _columns = {
+        'wbs_id': fields.many2one('project_pmi.wbs','WBS'),
         'code': fields.char('Code', size=20, required=True, select=True),
         'complete_name': fields.function(_name_get_fnc, type="char", string='Name'),
         'name': fields.char('Name', size=255, required=True, select=True),
+        'type': fields.selection([('work_package', 'Work Package'),('deliverable', 'Deliverable')],'Deliverable Type', required=True),
         'weight': fields.float('Weight'),
         'description': fields.text('Description'),
         'active':fields.boolean('Active',help='Enable/Disable'),
         'state':fields.selection([('draft', 'Draft'),('open', 'In Progress'),('cancel', 'Cancelled'),('done', 'Done'),('pending', 'Pending')],'State'),
-        'wbs_id': fields.many2one('project_pmi.wbs','WBS'),
-        'work_package_ids': fields.one2many('project_pmi.work_package', 'deliverable_id', string='Work Packages'),
+        'quantity': fields.float('Quantity'),
+        'uom_id': fields.many2one('product.uom', 'Unit of Measure', help="Default Unit of Measure used"),
         'parent_id': fields.many2one('project_pmi.deliverable','Parent Deliverable', select=True, ondelete='cascade'),
         'child_ids': fields.one2many('project_pmi.deliverable', 'parent_id', string='Child Deliverables'),
         'sequence': fields.integer('Sequence', select=True, help="Gives the sequence order when displaying a list."),
         'parent_left': fields.integer('Left Parent', select=1),
         'parent_right': fields.integer('Right Parent', select=1),
+        'color': fields.integer('Color Index'),
+        'date_deadline': fields.date('Deadline',select=True),
+        'date_start': fields.date('Starting Date',select=True),
+        'date_end': fields.date('Ending Date',select=True),
     }
     _defaults = {
         'active': True,
         'state': 'draft',
         'code': lambda obj, cr, uid, context: obj.pool.get('ir.sequence').get(cr, uid, 'project_pmi.deliverable')
     }
-    _parent_name = "parent_id"
-    _parent_store = True
-    _parent_order = 'sequence, name'
-    _order = 'parent_id DESC'
 
     def _check_recursion(self, cr, uid, ids, context=None):
         level = 100
@@ -138,26 +152,3 @@ class project_pmi_deliverable(osv.osv):
         return [ids]
 
 project_pmi_deliverable()
-
-class project_pmi_work_package(osv.osv):
-    _name = "project_pmi.work_package"
-    _inherit = ['mail.thread']
-    _description = "Defines a traceable Work Package in a WBS"
-    _columns = {
-        'code': fields.char('Code', size=20, required=True, select=True),
-        'name': fields.char('Name', size=255, required=True, select=True),
-        'description': fields.text('Description'),
-        'weight': fields.float('Weight', help="Weight related to the full WBS Delivery"),
-        'quantity': fields.float('Quantity'),
-        'uom_id': fields.many2one('product.uom', 'Unit of Measure', required=True, help="Default Unit of Measure used"),
-        'active':fields.boolean('Active',help='Enable/Disable'),
-        'state':fields.selection([('draft', 'Draft'),('open', 'In Progress'),('cancel', 'Cancelled'),('done', 'Done'),('pending', 'Pending')],'State'),
-        'deliverable_id': fields.many2one('project_pmi.deliverable','Deliverable'),
-    }
-    _defaults = {
-        'active': True,
-        'state': 'draft',
-        'code': lambda obj, cr, uid, context: obj.pool.get('ir.sequence').get(cr, uid, 'project_pmi.work_package')
-    }
-
-project_pmi_work_package()
