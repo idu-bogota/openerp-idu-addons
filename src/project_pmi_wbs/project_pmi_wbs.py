fb3e21f1628911239cf2a56e15bb1bd0b130f52f
Cinxgler Mariaca <cinxgler@gmail.com>
2013-12-17 18:28:05 -0500

    Display WBS progress

--- src/project_pmi_wbs/project_pmi_wbs.py
+++ src/project_pmi_wbs/project_pmi_wbs.py
@@ -19,13 +19,26 @@
 ##############################################################################
 
 from openerp.osv import fields, osv
+import time
 
 class project(osv.osv):
     _name = "project.project"
     _inherit = "project.project"
-    
+
+    def _wbs_item_count(self, cr, uid, ids, field_name, arg, context=None):
+        if context is None:
+            context = {}
+        res = dict.fromkeys(ids, 0)
+        ctx = context.copy()
+        ctx['active_test'] = False
+        item_ids = self.pool.get('project_pmi.wbs_item').search(cr, uid, [('project_id', 'in', ids)], context=ctx)
+        for item in self.pool.get('project_pmi.wbs_item').browse(cr, uid, item_ids, context):
+            res[item.project_id.id] += 1
+        return res
+
     _columns = {
         'wbs_ids': fields.one2many('project_pmi.wbs_item', 'project_id', string='Work Breakdown Structure'),
+        'wbs_items_count': fields.function(_wbs_item_count, type='integer', string="WBS items count", store=True),
     }
 
 project()
@@ -44,7 +57,7 @@ class project_pmi_wbs_item(osv.osv):
     _parent_name = "parent_id"
     _parent_store = True
     _parent_order = 'sequence, name'
-    _order = 'parent_id DESC'
+    _order = 'parent_right DESC'
 
     def name_get(self, cr, uid, ids, context=None):
         if isinstance(ids, (list, tuple)) and not len(ids):
@@ -63,6 +76,20 @@ class project_pmi_wbs_item(osv.osv):
     def _name_get_fnc(self, cr, uid, ids, prop, unknow_none, context=None):
         res = self.name_get(cr, uid, ids, context=context)
         return dict(res)
+
+    #FIXME: No hacer queries en loop para evitar problemas de rendimiento
+    def _progress_rate(self, cr, uid, ids, names, arg, context=None):
+        # compute progress rates
+        res = {}
+        for id in ids:
+            res[id] = {}
+            total_work = 0
+            wbs_item = self.pool.get('project_pmi.wbs_item').browse(cr, uid, id, context=context)
+            item_ids = self.pool.get('project_pmi.wbs_work_record').search(cr, uid, [('wbs_item_id', '=', id)], context=context)
+            for item in self.pool.get('project_pmi.wbs_work_record').browse(cr, uid, item_ids, context):
+                total_work += item.quantity
+            res[id]['progress_rate'] = round(100.0 * total_work / wbs_item.quantity, 2)
+        return res
 
     _columns = {
         'project_id': fields.many2one('project.project','Project'),
@@ -86,6 +113,9 @@ class project_pmi_wbs_item(osv.osv):
         'date_deadline': fields.date('Deadline',select=True),
         'date_start': fields.date('Starting Date',select=True),
         'date_end': fields.date('Ending Date',select=True),
+        'progress_rate': fields.function(_progress_rate, multi="progress", string='Progress', type='float', group_operator="avg", 
+             help="Percent of progress according to the total of work recorded.", store=True),
+        'work_record_ids': fields.one2many('project_pmi.wbs_work_record', 'wbs_item_id', 'Work done'),
     }
     _defaults = {
         'active': True,
@@ -111,3 +141,42 @@ class project_pmi_wbs_item(osv.osv):
         return [ids]
 
 project_pmi_wbs_item()
+
+class project_pmi_wbs_work_record(osv.osv):
+    _name = "project_pmi.wbs_work_record"
+    _description = "WBS Item Work Record"
+    _columns = {
+        'name': fields.char('Work summary', size=128),
+        'date': fields.datetime('Date', select="1"),
+        'wbs_item_id': fields.many2one('project_pmi.wbs_item', 'Task', ondelete='cascade', required=True, select="1"),
+        'quantity': fields.float('Quantity'),
+        'user_id': fields.many2one('res.users', 'Done by', required=True, select="1"),
+    }
+
+    _defaults = {
+        'user_id': lambda obj, cr, uid, context: uid,
+        'date': lambda *a: time.strftime('%Y-%m-%d %H:%M:%S')
+    }
+
+    _order = "date desc"
+    def create(self, cr, uid, vals, *args, **kwargs):
+        if 'quantity' in vals and (not vals['quantity']):
+            vals['quantity'] = 0.00
+        #TODO: Aplicar calculo de trabajo pendiente en el wbs item
+        #if 'wbs_item_id' in vals:
+        #    cr.execute('update project_task set remaining_hours=remaining_hours - %s where id=%s', (vals.get('hours',0.0), vals['task_id']))
+        return super(project_pmi_wbs_work_record,self).create(cr, uid, vals, *args, **kwargs)
+
+    def write(self, cr, uid, ids, vals, context=None):
+        if 'quantity' in vals and (not vals['quantity']):
+            vals['quantity'] = 0.00
+        #TODO: Aplicar calculo de trabajo pendiente en el wbs item
+        #if 'wbs_item_id' in vals:
+        #    cr.execute('update project_task set remaining_hours=remaining_hours - %s where id=%s', (vals.get('hours',0.0), vals['task_id']))
+        return super(project_pmi_wbs_work_record,self).write(cr, uid, ids, vals, context)
+
+    def unlink(self, cr, uid, ids, *args, **kwargs):
+        #TODO: Aplicar calculo de trabajo pendiente en el wbs item
+        #for work in self.browse(cr, uid, ids):
+        #    cr.execute('update project_task set remaining_hours=remaining_hours + %s where id=%s', (work.hours, work.task_id.id))
+        return super(project_pmi_wbs_work_record,self).unlink(cr, uid, ids,*args, **kwargs)
