59641640b0b1046e9bc63db98d8bf107bb17313a
Cinxgler Mariaca Minda <cinxgler@gmail.com>
2013-12-18 19:39:40 -0500

    Fix en project_gtd_extra

--- src/project_gtd_extra/__init__.py
+++ src/project_gtd_extra/__init__.py
@@ -18,6 +18,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
 #
 ##############################################################################
-import project_gtd_extra
+#import project_gtd_extra
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
