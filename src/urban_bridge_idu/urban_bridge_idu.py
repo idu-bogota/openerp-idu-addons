57bfe0996e62ca6afaf89ca8c94d7fb4cc8ec96d
Andres Ignacio Baez Alba <andresignaciob@gmail.com>
2013-12-24 10:06:08 -0500

    Arreglando bug en if de seleccion de tipo de dato en wizard

--- src/urban_bridge_idu/urban_bridge_idu.py
+++ src/urban_bridge_idu/urban_bridge_idu.py
@@ -61,7 +61,8 @@ class urban_bridge_bridge(geo_model.GeoModel):
                 y actualice el módulo")
             if (tipo_dato=="selection"):
                 puente[field] = {'nom_pres':nombre_presentacion,"tipo_dato":tipo_dato,"nombre":campo,"selection":fields[field]["selection"]}
-            if (tipo_dato == "many2one"):
+            #Convertir los elementos many 2 one en un dominio tipo selection
+            elif (tipo_dato == "many2one"):
                 objeto_rel = self.pool.get(fields[field]["relation"])
                 try:
                     objetos_rel_ids = objeto_rel.search(cr,uid,[('id','>','0')])
@@ -82,6 +83,7 @@ class urban_bridge_bridge(geo_model.GeoModel):
         common_fields={}
         for field in fields:
             campo = translate(cr,"addons/urban_bridge/urban_bridge.py","code","es_CO",field)
+            #Nombre con el cual se ve el campo en la pantalla
             nombre_presentacion = translate(cr,"urban_bridge.structure_element,"+field,"field","es_CO",fields[field]["string"])
             if (nombre_presentacion==False):
                 nombre_presentacion = fields[field]["string"]
@@ -97,8 +99,8 @@ class urban_bridge_bridge(geo_model.GeoModel):
         for struct_elem_type in struct_elem_type_obj.browse(cr,uid,struct_elem_type_ids):
             objeto = struct_elem_type.alias
             objeto_dict = {}
-            for field in common_fields:
-                objeto_dict[field]=fields[field]
+            for comfield in common_fields:
+                objeto_dict[field]=common_fields[comfield]
             for attribute in struct_elem_type.attributes:
                 if (attribute.data_type=="selection"):
                     objeto_dict[attribute.alias]={"nombre":str(attribute.alias),'nom_pres':str(attribute.name),"tipo_dato":str(attribute.data_type), "selection":str(attribute.selection_text)}
