98c8ea43e3bf972376185226718e85d6da266b0a
Vagrant Account <vagrant@a01814-vm01.(none)>
2014-02-17 16:24:16 -0500

    Se ajusta el xml importer para que ultimo nodo del xml sea tarea

--- src/project_pmi_wbs/wizard/create_wbs_from_file.py
+++ src/project_pmi_wbs/wizard/create_wbs_from_file.py
@@ -62,6 +62,10 @@ class project_pmi_wbs_wizard_create_wbs_from_file(osv.osv_memory):
             struct[task.find('{http://schemas.microsoft.com/project}OutlineNumber').text] = father
         return struct
 
+    #flag must always be 1
+    #return == 1 is Task
+    #       == 0 is Package
+    #       == -1 is Deliverable  
     def get_chlids(self,struct,id,flag):
         for key in struct:
             if struct[key] == id:
@@ -73,47 +77,69 @@ class project_pmi_wbs_wizard_create_wbs_from_file(osv.osv_memory):
 
     def action_create(self, cr, uid, ids, context=None):
         wizards = self.pool.get('project_pmi_wbs.wizard.create_wbs_from_file').browse(cr,uid,ids,context=None)
+        struct = {}
         for wizard in wizards:
             tree = ET.XML(base64.decodestring(wizard.file))
-            struct = self.put_tree_on_struct(tree)
-            flag = self.get_chlids(struct, '1.1.10.1', 1)
             date_project = tree.find('{http://schemas.microsoft.com/project}StartDate').text
             add_days = self.calculate_days(date_project)
             parent_ids = [0]
-            return {'type': 'ir.actions.act_window_close'}
+            if wizard.take_leaves_as_tasks:
+                struct = self.put_tree_on_struct(tree)
+#             return {'type': 'ir.actions.act_window_close'}
             for task in tree.iter('{http://schemas.microsoft.com/project}Task'):
                 data_task_create = False
                 outline_level = int(task.find('{http://schemas.microsoft.com/project}OutlineLevel').text)
                 name = task.find('{http://schemas.microsoft.com/project}Name').text
+                outline_number = task.find('{http://schemas.microsoft.com/project}OutlineNumber').text
                 if wizard.include_wbs_outline_number:
-                    outline_number = task.find('{http://schemas.microsoft.com/project}OutlineNumber').text
                     name = '{0} {1}'.format(outline_number, name)
-                if outline_level <= wizard.max_level_evaluate:
+                if outline_level <= wizard.max_level_evaluate and outline_level > 0:
                     data = {'name':name,
                             'parent_id':parent_ids[outline_level -1],
                             }
-                    if outline_level < wizard.min_level_task - 1:
-                        data['type'] = 'deliverable'
-                    elif outline_level == wizard.min_level_task - 1:
-                        date_start = self.get_date(task.find('{http://schemas.microsoft.com/project}Start').text) + timedelta(days=add_days)
-                        date_deadline = self.get_date(task.find('{http://schemas.microsoft.com/project}Finish').text) - timedelta(days=add_days)
-                        data['type'] = 'work_package'
-                        data['tracking_type'] = 'tasks'
-                        data['date_start'] = date_start.strftime('%Y-%m-%d')
-                        data['date_deadline'] = date_deadline.strftime('%Y-%m-%d')
+                    if wizard.take_leaves_as_tasks:
+                        have_grandson = self.get_chlids(struct, outline_number, 1)
+                        if have_grandson == -1:
+                            data['type'] = 'deliverable'
+                        elif have_grandson == 0:
+                            date_start = self.get_date(task.find('{http://schemas.microsoft.com/project}Start').text) + timedelta(days=add_days)
+                            date_deadline = self.get_date(task.find('{http://schemas.microsoft.com/project}Finish').text) - timedelta(days=add_days)
+                            data['type'] = 'work_package'
+                            data['tracking_type'] = 'tasks'
+                            data['date_start'] = date_start.strftime('%Y-%m-%d')
+                            data['date_deadline'] = date_deadline.strftime('%Y-%m-%d')
+                        else:
+                            data_task = {'name':name,
+                                      'wbs_item_id':parent_ids[outline_level -1],
+                                      'planned_quantity':100.0,
+                                      'effective_quantity':100.0,
+                                     }
+                            self.pool.get('project.task').create(cr, uid, data_task, context)
+                            data_task_create = True
+                        if not data_task_create:
+                            parent_ids.insert(outline_level, self.pool.get('project_pmi.wbs_item').create(cr, uid, data, context))
                     else:
-                        data_task = {'name':name,
+                        if outline_level < wizard.min_level_task - 1:
+                            data['type'] = 'deliverable'
+                        elif outline_level == wizard.min_level_task - 1:
+                            date_start = self.get_date(task.find('{http://schemas.microsoft.com/project}Start').text) + timedelta(days=add_days)
+                            date_deadline = self.get_date(task.find('{http://schemas.microsoft.com/project}Finish').text) - timedelta(days=add_days)
+                            data['type'] = 'work_package'
+                            data['tracking_type'] = 'tasks'
+                            data['date_start'] = date_start.strftime('%Y-%m-%d')
+                            data['date_deadline'] = date_deadline.strftime('%Y-%m-%d')
+                        else:
+                            data_task = {'name':name,
                                      'wbs_item_id':parent_ids[outline_level -1],
                                      'planned_quantity':100.0,
                                      'effective_quantity':100.0,
                                     }
-                        if not wizard.assign_task_to_current_user:
-                            data_task['user_id'] = None
-
-                        self.pool.get('project.task').create(cr, uid, data_task, context)
-                        data_task_create = True
-                    if not data_task_create:
-                        parent_ids.insert(outline_level, self.pool.get('project_pmi.wbs_item').create(cr, uid, data, context))
+                            if not wizard.assign_task_to_current_user:
+                                data_task['user_id'] = None
+                            self.pool.get('project.task').create(cr, uid, data_task, context)
+                            data_task_create = True
+                        if not data_task_create:
+                            parent_ids.insert(outline_level, self.pool.get('project_pmi.wbs_item').create(cr, uid, data, context))
 #                     print outline_number + ' - '  + name
         return {'type': 'ir.actions.act_window_close'}
 
