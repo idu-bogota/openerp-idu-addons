3214c11eba5fd6a247076ef2199578e2c5b55f23
Cinxgler Mariaca <cinxgler@gmail.com>
2014-02-27 19:05:41 -0500

    Seleccionar dependencia del usuario por defecto en plan item

--- src/plan_contratacion_idu/plan_contratacion_idu.py
+++ src/plan_contratacion_idu/plan_contratacion_idu.py
@@ -556,9 +556,13 @@ class plan_contratacion_idu_item(osv.osv):
             store = True),
     }
 
+    def _default_dependencia_id(self, cr, uid, context):
+        return self.pool.get('res.users').browse(cr, uid, uid, context=context).department_ids
+
     _defaults = {
         'state': 'draft',
         'progress_rate':0,
+        'dependencia_id': _default_dependencia_id,
         #Asigna por defecto el plan contractual pasado en el contexto
         'plan_id' : lambda self, cr, uid, context : context['plan_id'] if context and 'plan_id' in context else None,
     }
