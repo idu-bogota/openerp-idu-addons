ed83b3942afc921bf5501d2ec509097d47fbf138
Julian Andres Fernandez <juanfedo@gmail.com>
2014-03-20 10:25:42 -0500

    Se creaun css particular del instituto

---
+++ src/theme_idu/__init__.py
@@ -0,0 +1,1 @@
+from openerp.modules import get_module_resource, get_module_path
---
+++ src/theme_idu/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Theme IDU',
+    'category': 'Hidden',
+    'description': """
+OpenERP ccs style.
+=============================
+
+""",
+    'version': '2.0',
+    'depends': ['web'],
+    'css': ['static/src/css/theme_idu.css'],
+    'auto_install': True
+}
